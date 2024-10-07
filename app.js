d387e99db97b0485ed72ec4191d18b6554505d18
Ombhabal <143935443+Ombhabal@users.noreply.github.com>
2024-10-07 19:02:09 +0530

    app.js

--- app.js
+++ app.js
@@ -21,6 +21,6 @@ app.use("/api/test", testRoute);
 app.use("/api/chats", chatRoute);
 app.use("/api/messages", messageRoute);
 
-app.listen(8800, () => {
+app.listen(https://real-estate-backend-e5cn.onrender.com/, () => {
   console.log("Server is running!");
 });
